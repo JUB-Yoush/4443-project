3b063447b889834977d6ce9a34cdf3ffa43ba4ff
JUB_Yoush <yoush1337@gmail.com>
2025-11-12 20:42:31 -0500

    login and signup pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 //import './login.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  Widget firstPage = LoginPage();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: LoginPage());
+    return MaterialApp(
+      home: FutureBuilder(
+        future: getLoginPrefs(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            return snapshot.data == true ? HomePage() : LoginPage();
+          }
+        },
+      ),
+    );
+  }
+
+  Future<bool> getLoginPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('is_logged_in') ?? false;
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("Money App")));
   }
 }
 
@@ -16,6 +47,90 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  Future<void> setLoginPrefs(bool value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('is_logged_in', value);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'username invalid';
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                icon: Icon(Icons.person),
+                hintText: "username",
+                labelText: "Name *",
+              ),
+            ),
+            TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'password invalid';
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                icon: Icon(Icons.person),
+                hintText: "password",
+                labelText: "password *",
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignupPage()),
+                );
+              },
+              child: Text('Signup'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  // TODO store and validate input somehow
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Login Successful')),
+                  );
+                  //setLoginPrefs(true); disabled for testing
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomePage()),
+                  );
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SignupPage extends StatefulWidget {
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
   final _formKey = GlobalKey<FormState>();
 
   @override
@@ -32,6 +147,38 @@ class _LoginPageState extends State<LoginPage> {
         child: Column(
           children: <Widget>[
             TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Name cannot be empty';
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                icon: Icon(Icons.person),
+                hintText: "First Name",
+                labelText: "First Name *",
+              ),
+            ),
+            TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Name cannot be empty';
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                icon: Icon(Icons.person),
+                hintText: "Last Name",
+                labelText: "Last Name *",
+              ),
+            ),
+            TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'username invalid';
+                }
+                return null;
+              },
               decoration: const InputDecoration(
                 icon: Icon(Icons.person),
                 hintText: "username",
@@ -39,15 +186,45 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ),
             TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'username invalid';
+                }
+                return null;
+              },
               decoration: const InputDecoration(
                 icon: Icon(Icons.person),
                 hintText: "password",
                 labelText: "password *",
               ),
             ),
+
             ElevatedButton(
               onPressed: () {
-                print("pressed");
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              },
+              child: Text('Login'),
+            ),
+
+            ElevatedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_formKey.currentState!.validate()) {
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Account Created, Please Log in'),
+                    ),
+                  );
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                }
               },
               child: Text('Submit'),
             ),
