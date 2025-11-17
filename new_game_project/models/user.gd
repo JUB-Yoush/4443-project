3da1a191862500e5106fa8b7c1df7080ff822a0d
JUB_Yoush <yoush1337@gmail.com>
2025-11-17 13:32:30 -0500

    home screen

--- new_game_project/models/user.gd
+++ new_game_project/models/user.gd
@@ -1,9 +1,12 @@
-extends Node
+extends RefCounted
 class_name User
 
 var person_name:Name
 var username:String
 var password:String
+
+var goals: Array[Goal]
+
 
 # static func validate(fname:String lname:string) -> bool:
 #     if user.
