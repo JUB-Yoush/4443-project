90512d5b4b4f6f8418088f40b078ea6dda81f2d3
JUB_Yoush <yoush1337@gmail.com>
2025-11-18 21:55:34 -0500

    partially implemented goal entry form

---
+++ new_game_project/controllers/saving_goal_maker.gd
@@ -0,0 +1,66 @@
+extends ScrollContainer
+
+enum SavingsType{
+SPENDING, 
+REGULAR,
+DEPOSIT
+}
+
+enum SavingsFreq{
+	NONE,
+	DAILY,
+	WEEKLY,
+	MONTHLY,
+}
+
+@onready var spendingWidget = $%SpendingSave
+@onready var regularWidget = $%RegularSave
+@onready var depositWidget = $%DepositSave
+
+var current_trigger := SavingsType.SPENDING
+
+var freq := SavingsFreq.DAILY
+
+var percentage_saved = 0
+var regular_savings_amount := 0.0
+var goal_name := ""
+
+var datetime:Dictionary 
+
+@onready var datePickerPanel := %DatePickerPanel
+
+func _ready():
+	change_trigger(SavingsType.SPENDING)
+	$%DailySave.button_pressed = true
+
+	$%SpendingSaveBtn.pressed.connect(func(): change_trigger(SavingsType.SPENDING))
+	$%RegularSaveBtn.pressed.connect(func(): change_trigger(SavingsType.REGULAR))
+	$%DepositSaveBtn.pressed.connect(func(): change_trigger(SavingsType.DEPOSIT))
+	
+	$%SpendingSlider.value_changed.connect(func(value:float): 
+		percentage_saved = int(value)
+		$%SpendingLabel.text = "Save " + str(int(value)) + "% of payout automatically" )
+
+	$%DepositSlider.value_changed.connect(func(value:float): 
+		percentage_saved = int(value)
+		$%DepositLabel.text = "Save " + str(int(value)) + "% of payout automatically" 
+		)
+	
+	$%DailySave.pressed.connect(func(): freq = SavingsFreq.DAILY)
+	$%WeeklySave.pressed.connect(func(): freq = SavingsFreq.WEEKLY)
+	$%MonthlySave.pressed.connect(func(): freq = SavingsFreq.MONTHLY)
+
+	$%SavingAmountEntry.value_changed.connect(func(value:float): regular_savings_amount = value)
+	
+	$%GoalNameEntry.text_changed.connect(func(value:String): goal_name = value )
+
+func change_trigger(new_trigger:SavingsType) -> void:
+	current_trigger = new_trigger
+	spendingWidget.visible = SavingsType.SPENDING == current_trigger
+	regularWidget.visible = SavingsType.REGULAR == current_trigger
+	depositWidget.visible = SavingsType.DEPOSIT == current_trigger
+
+func validate() -> bool:
+	datetime = datePickerPanel.get_date_data()
+			
+	return true
--- new_game_project/models/goal.gd
+++ new_game_project/models/goal.gd
@@ -1,8 +1,16 @@
 extends Resource
 class_name Goal
 
+enum SavingsType{
+   SPENDING, 
+   REGULAR,
+   DEPOSIT
+}
+
 var goal_name:String
 var end_date:String
 var goal_amount:int
 var current_amount:int
 var icon:Texture2D
+
+
