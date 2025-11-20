273da4acbda1d86afbb46bc6514b8c153a93e1d2
JUB_Yoush <yoush1337@gmail.com>
2025-11-20 17:05:44 -0500

    fixed profile return

--- new_game_project/controllers/profile.gd
+++ new_game_project/controllers/profile.gd
@@ -1,11 +1,10 @@
 extends Control
-@onready var return_btn:Button = $PanelContainer/MarginContainer/VBoxContainer/Button2
 
 func _ready() -> void:
 	$%LogoutButton.pressed.connect(logout)
 	$%NameLabel.text = "[b]"+ DBController.get_logged_in_user().get_users_name()+ "[/b]"
+	$%Return.pressed.connect(func(): get_tree().change_scene_to_file("res://views/home.tscn"))
 
 func logout() -> void:
 	DBController.logout()
 	get_tree().change_scene_to_file("res://views/login.tscn")
-	return_btn.pressed.connect(func(): get_tree().change_scene_to_file("res://views/home.tscn"))
