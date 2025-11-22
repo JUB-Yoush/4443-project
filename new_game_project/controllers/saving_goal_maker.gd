71f74a8b4753fbb2eeb2c7e1e1c9e0346e528fb1
JUB_Yoush <yoush1337@gmail.com>
2025-11-21 20:49:26 -0500

    buttons

--- new_game_project/controllers/saving_goal_maker.gd
+++ new_game_project/controllers/saving_goal_maker.gd
@@ -21,8 +21,15 @@ var current_trigger := SavingsType.SPENDING
 
 var freq := SavingsFreq.DAILY
 
-var percentage_saved = 0
-var regular_savings_amount := 0.0
+var percentage_saved = 2:
+	set(new_value):
+		percentage_saved = new_value
+		$%SpendingLabel.text = "Save " + str(int(new_value)) + "% of payout automatically" 
+		
+var regular_savings_amount := 2.0:
+	set(new_value):
+		regular_savings_amount = new_value
+		$%DepositLabel.text = "Save " + str(int(new_value)) + "% of payout automatically" 
 var goal_amount := 0.0
 var goal_name := ""
 
@@ -52,12 +59,44 @@ func _ready():
 	$%MonthlySave.pressed.connect(func(): freq = SavingsFreq.MONTHLY)
 
 	$%SavingAmountEntry.value_changed.connect(func(value:float): regular_savings_amount = value)
+
 	$%GoalAmountEntry.value_changed.connect(func(value:float): goal_amount = value)
 	
 	$%GoalNameEntry.text_changed.connect(func(value:String): goal_name = value )
 	
 	$%Submit.pressed.connect(submit)
 	$%Return.pressed.connect(func(): get_tree().change_scene_to_file("res://views/home.tscn"))
+	
+	$%SaveOther.pressed.connect(func():
+		$%OtherPercentageEntry.visible = !$%OtherPercentageEntry.visible
+		)
+		
+	$%OtherPercentageEntry.value_changed.connect(func(value:float): percentage_saved = value)
+
+	$%Save2.pressed.connect(func(): percentage_saved = 2)
+	$%Save5.pressed.connect(func(): percentage_saved = 5)
+	$%Save10.pressed.connect(func(): percentage_saved = 10)
+
+	$%Save2r.pressed.connect(func(): 
+		regular_savings_amount= 2
+		$%OtherPercentageEntry.visible = false
+		)
+	$%Save5r.pressed.connect(func():
+		regular_savings_amount= 5
+		$%OtherPercentageEntry.visible = false
+		)
+	$%Save10r.pressed.connect(func(): 
+		regular_savings_amount= 10
+
+		$%OtherPercentageEntry.visible = false
+	)
+	
+	$%SaveOtherr.pressed.connect(func():
+		$%OtherPercentageEntryr.visible = !$%OtherPercentageEntryr.visible
+		)
+		
+	$%OtherPercentageEntryr.value_changed.connect(func(value:float): regular_savings_amount= value)
+	
 
 func change_trigger(new_trigger:SavingsType) -> void:
 	current_trigger = new_trigger
